284e14e99631c450a6a7a46fe495e44c50a80327
Rémi GRISOT <remi.grisot@isen.yncrea.fr>
2023-02-14 14:47:06 +0100

    Creates basic fibonacci impl
    Defines a recursive function to compute the fibonacci value
    for a given integer

---
+++ src/fibonacci/fibonacci.py
@@ -0,0 +1,12 @@
+from functools import lru_cache
+
+
+@lru_cache(maxsize=1000)
+def fibonacci(n):
+    return 1 if (n == 1 or n == 2) else fibonacci(n - 1) + fibonacci(n - 2)
+
+
+if __name__ == '__main__':
+    for i in range(1, 100):
+        print(f"fibonacci({i}) = {fibonacci(i)}")
+
